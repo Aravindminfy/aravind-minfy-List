a33d58946febfce8a1e8aac7f56eb0ded81da3af
Aravindminfy <aravind.appusamy@minfytech.com>
2025-05-19 14:54:39 +0530

    Update list_solutions.py

--- list_solutions.py
+++ list_solutions.py
@@ -4,7 +4,7 @@ def filter_even_numbers(num):
     return [i for i in num if i%2==0]
 
 # Example usage:
-print(filter_even_numbers([1, 2, 3, 4, 5, 6]))  # Should return [2, 4, 6]
+print(filter_even_numbers([1, 2, 3, 4, 10, 6]))  # Should return [2, 4, 6]
 print(filter_even_numbers([1, 3, 5]))  # Should return []
 
 #---------------------------------------------------------------------
